15e5c969a18582895ded2738ed0db1e4784e82fe
Maxwell Koo <mjkoo90@gmail.com>
2018-04-30 05:34:02 -0700

    Update pre 4/30 milestone

--- scripts/build_elf.sh
+++ scripts/build_elf.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 pyinstaller polyswarmd.py -y --clean
-mkdir -p dist/polyswarmd/frontend
-cp -r frontend/build/ dist/polyswarmd/frontend/
 mkdir -p dist/polyswarmd/truffle
 cp -r truffle/build/ dist/polyswarmd/truffle/
 cp polyswarmd.cfg dist/polyswarmd/
