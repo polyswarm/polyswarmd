f883a398ce23fa98cec4d30df3745d6b49537130
Rob Lathrop <rl@polyswarm.io>
2018-07-10 13:18:08 -0400

    Add staking deposit, withdrawal, and balance checks.

--- src/polyswarmd/balances.py
+++ src/polyswarmd/balances.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 
-from polyswarmd.eth import web3 as web3_chains, nectar_token as nectar_chains
+from polyswarmd.eth import web3 as web3_chains, nectar_token as nectar_chains, arbiter_staking as arbiter_chains
 from polyswarmd.response import success, failure
 
 balances = Blueprint('balances', __name__)
@@ -23,6 +23,48 @@ def get_balance_address_eth(address):
     address = web3.toChecksumAddress(address)
     try:
         balance = web3.eth.getBalance(address)
+        return success(str(balance))
+    except:
+        return failure("Could not retrieve balance")
+
+@balances.route('/<address>/staking/total', methods=['GET'])
+def get_balance_total_stake(address):
+    chain = request.args.get('chain')
+    if not chain:
+        chain = 'home'
+    elif chain != 'side' and chain != 'home':
+        return failure('Chain must be either home or side', 400)
+
+    web3 = web3_chains[chain]
+    arbiter_staking = arbiter_chains[chain]
+
+    if not web3.isAddress(address):
+        return failure('Invalid address', 400)
+    address = web3.toChecksumAddress(address)
+    try:
+        balance = arbiter_staking.functions.balanceOf(address).call()
+        return success(str(balance))
+    except:
+        return failure("Could not retrieve balance")
+
+
+@balances.route('/<address>/staking/withdrawable', methods=['GET'])
+def get_balance_withdrawable_stake(address):
+    chain = request.args.get('chain')
+    if not chain:
+        chain = 'home'
+    elif chain != 'side' and chain != 'home':
+        return failure('Chain must be either home or side', 400)
+
+    web3 = web3_chains[chain]
+    arbiter_staking = arbiter_chains[chain]
+
+    if not web3.isAddress(address):
+        return failure('Invalid address', 400)
+
+    address = web3.toChecksumAddress(address)
+    try:
+        balance = arbiter_staking.functions.withdrawableBalanceOf(address).call()
         return success(str(balance))
     except:
         return failure("Could not retrieve balance")
---
+++ src/polyswarmd/staking.py
@@ -0,0 +1,118 @@
+import jsonschema
+
+from flask import Blueprint, request
+from jsonschema.exceptions import ValidationError
+
+from polyswarmd import eth
+from polyswarmd.eth import web3 as web3_chains, build_transaction, nectar_token as nectar_chains, arbiter_staking as arbiter_chains
+from polyswarmd.response import success, failure
+
+staking = Blueprint('staking', __name__)
+
+
+@staking.route('/deposit', methods=['POST'])
+def post_arbiter_staking_deposit():
+    # Must read chain before account to have a valid web3 ref
+    chain = request.args.get('chain', 'home')
+    if chain != 'side' and chain != 'home':
+        return failure('Chain must be either home or side', 400)
+
+    web3 = web3_chains[chain]
+    nectar_token = nectar_chains[chain]
+    arbiter_staking = arbiter_chains[chain]
+
+    account = request.args.get('account')
+    if not account or not web3.isAddress(account):
+        return failure('Source account required', 401)
+    account = web3.toChecksumAddress(account)
+
+    base_nonce = int(
+        request.args.get('base_nonce', web3.eth.getTransactionCount(account)))
+
+    schema = {
+        'type': 'object',
+        'properties': {
+            'amount': {
+                'type': 'string',
+                'minLength': 1,
+                'maxLength': 100,
+                'pattern': r'^\d+$',
+            }
+        },
+        'required': ['amount'],
+    }
+
+    body = request.get_json()
+    try:
+        jsonschema.validate(body, schema)
+    except ValidationError as e:
+        return failure('Invalid JSON: ' + e.message, 400)
+
+    amount = int(body['amount'])
+
+    total = arbiter_staking.functions.balanceOf(account).call()
+
+    if amount + total >= eth.staking_total_max():
+        return failure('Total stake above allowable maximum.', 400)
+
+    transactions = [
+        build_transaction(
+            nectar_token.functions.approve(arbiter_staking.address, amount), chain, base_nonce),
+        build_transaction(
+            arbiter_staking.functions.deposit(amount), chain, base_nonce + 1),
+    ]
+
+    return success({'transactions': transactions})
+
+
+@staking.route('/withdraw', methods=['POST'])
+def post_arbiter_staking_withdrawal():
+    # Must read chain before account to have a valid web3 ref
+    chain = request.args.get('chain', 'home')
+    if chain != 'side' and chain != 'home':
+        return failure('Chain must be either home or side', 400)
+
+    web3 = web3_chains[chain]
+    nectar_token = nectar_chains[chain]
+    arbiter_staking = arbiter_chains[chain]
+
+    account = request.args.get('account')
+    if not account or not web3.isAddress(account):
+        return failure('Source account required', 401)
+    account = web3.toChecksumAddress(account)
+
+    base_nonce = int(
+        request.args.get('base_nonce', web3.eth.getTransactionCount(account)))
+
+    schema = {
+        'type': 'object',
+        'properties': {
+            'amount': {
+                'type': 'string',
+                'minLength': 1,
+                'maxLength': 100,
+                'pattern': r'^\d+$',
+            }
+        },
+        'required': ['amount'],
+    }
+
+    body = request.get_json()
+    try:
+        jsonschema.validate(body, schema)
+    except ValidationError as e:
+        return failure('Invalid JSON: ' + e.message, 400)
+
+    amount = int(body['amount'])
+
+    available = arbiter_staking.functions.withdrawableBalanceOf(account).call()
+
+    if amount > available:
+        return failure('Exceeds withdrawal eligible %s' % available, 400)
+
+    transactions = [
+        build_transaction(
+            arbiter_staking.functions.withdraw(amount), chain, base_nonce),
+    ]
+
+    return success({'transactions': transactions})
