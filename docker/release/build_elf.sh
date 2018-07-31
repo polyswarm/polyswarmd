945b66617051cb8b6cb10bf07df715a401dc9e32
Maxwell Koo <mjkoo90@gmail.com>
2018-07-31 14:03:17 -0700

    Release image docker file and CI setup

--- docker/release/build_elf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pyinstaller src/polyswarmd/__main__.py -n polyswarmd -y --clean
-mkdir -p dist/polyswarmd/truffle
-cp -r truffle/build/ dist/polyswarmd/truffle/
-cp -r src/polyswarmd/config dist/polyswarmd/
-rm -rf polyswarmd.spec build
