f75076215fdeced50d162642b11ab2429fe8d69b
Colin Seale <ceseale3@gmail.com>
2018-11-19 14:02:53 -0800

    updated consul test

--- tests/test_consul.py
+++ tests/test_consul.py
@@ -2,14 +2,8 @@ import io
 
 import requests_mock
 
+from polyswarmd.config import Config
 
-def test_post_artifacts():
-    global consul_url
-    consul_url = "http://localhost:1600/v1/kv/gamma/"
-
-    from polyswarmd import config
-    config.consul_url = consul_url
+def test_from_consul_configs():
+    Config.from_consul()
     pass
-
-if __name__ == "__main__":
-    test_post_artifacts(None)
