ebe86d00610a00e963a62a38158722720e2bc3cc
Maxwell Koo <mjkoo90@gmail.com>
2018-12-17 16:00:02 -0700

    Monkey-patch our dependencies to cooperate with gevent coroutine model

--- src/polyswarmd/__init__.py
+++ src/polyswarmd/__init__.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 
+# Monkey patch for gevent compat
+from gevent import monkey
+monkey.patch_all()
+
+from psycogreen.gevent import patch_psycopg
+patch_psycopg()
+
+import requests
+import web3.utils
+
+# When using gevent web3 is exhausting connection pool, think due to unnecessary session caching layer over requests
+def patched_make_post_request(endpoint_uri, data, *args, **kwargs):
+    kwargs.setdefault('timeout', 10)
+    response = requests.post(endpoint_uri, data=data, *args, **kwargs)
+    response.raise_for_status()
+
+    return response.content
+
+web3.utils.make_post_request = patched_make_post_request
+
 import datetime
 import logging
 import os
