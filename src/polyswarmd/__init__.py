69b31e1fdde3bf73e9f743f409fc7d2696cc7ba4
Maxwell Koo <mjkoo90@gmail.com>
2018-12-17 16:20:44 -0700

    Refactor monkey patches into own module

--- src/polyswarmd/__init__.py
+++ src/polyswarmd/__init__.py
@@ -1,24 +1,6 @@
-#!/usr/bin/env python3
+from polyswarmd.monkey import patch_all
 
-# Monkey patch for gevent compat
-from gevent import monkey
-monkey.patch_all()
-
-from psycogreen.gevent import patch_psycopg
-patch_psycopg()
-
-import requests
-import web3.utils
-
-# When using gevent web3 is exhausting connection pool, think due to unnecessary session caching layer over requests
-def patched_make_post_request(endpoint_uri, data, *args, **kwargs):
-    kwargs.setdefault('timeout', 10)
-    response = requests.post(endpoint_uri, data=data, *args, **kwargs)
-    response.raise_for_status()
-
-    return response.content
-
-web3.utils.make_post_request = patched_make_post_request
+patch_all()
 
 import datetime
 import logging
