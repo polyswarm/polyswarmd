02dbbb8e4c936fa0216f13fa1c5b62e317ecab75
Maxwell Koo <mjkoo90@gmail.com>
2018-12-17 11:39:05 -0700

    Log error reponses from ipfs if request fails (#143)

--- src/polyswarmd/artifacts.py
+++ src/polyswarmd/artifacts.py
@@ -29,13 +29,15 @@ def is_valid_ipfshash(ipfshash):
 
 
 def list_artifacts(ipfshash):
+    r = None
     try:
         ipfs_uri = app.config['POLYSWARMD'].ipfs_uri
         r = requests.get(ipfs_uri + '/api/v0/ls', params={'arg': ipfshash}, timeout=1)
         r.raise_for_status()
         j = r.json()
     except Exception:
-        logger.exception('Received error listing files on IPFS')
+        logger.exception('Received error listing files on IPFS, got response: %s',
+                         r.content if r is not None else 'None')
         return []
 
     links = [(l['Name'], l['Hash'], l['Size']) for l in j['Objects'][0]['Links']]
@@ -47,12 +49,13 @@ def list_artifacts(ipfshash):
 
 @artifacts.route('/status', methods=['GET'])
 def get_artifacts_status():
+    r = None
     try:
         ipfs_uri = app.config['POLYSWARMD'].ipfs_uri
         r = requests.get(ipfs_uri + '/api/v0/diag/sys', timeout=1)
         r.raise_for_status()
     except Exception:
-        logger.exception('Received error connecting to IPFS')
+        logger.exception('Received error connecting to IPFS, got response: %s', r.content if r is not None else 'None')
         return failure('Could not connect to IPFS', 500)
 
     online = r.json()['net']['online']
@@ -66,6 +69,7 @@ def post_artifacts():
     if len(files) > 256:
         return failure('Too many artifacts', 400)
 
+    r = None
     try:
         ipfs_uri = app.config['POLYSWARMD'].ipfs_uri
         r = requests.post(
@@ -74,7 +78,7 @@ def post_artifacts():
             params={'wrap-with-directory': True})
         r.raise_for_status()
     except Exception:
-        logger.exception('Received error posting to IPFS')
+        logger.exception('Received error posting to IPFS got response: %s', r.content if r is not None else 'None')
         return failure('Could not add artifacts to IPFS', 400)
 
     ipfshash = json.loads(r.text.splitlines()[-1])['Hash']
@@ -111,12 +115,14 @@ def get_artifacts_ipfshash_id(ipfshash, id_):
     if size > MAX_ARTIFACT_SIZE:
         return failure('Artifact size greater than maximum allowed')
 
+    r = None
     try:
         ipfs_uri = app.config['POLYSWARMD'].ipfs_uri
         r = requests.get(ipfs_uri + '/api/v0/cat', params={'arg': artifact}, timeout=1)
         r.raise_for_status()
     except Exception:
-        logger.exception('Received error retrieving files from IPFS')
+        logger.exception('Received error retrieving files from IPFS, got response: %s',
+                         r.content if r is not None else 'None')
         return failure('Could not locate IPFS resource', 404)
 
     return r.content
@@ -136,12 +142,14 @@ def get_artifacts_ipfshash_id_stat(ipfshash, id_):
 
     artifact = arts[id_][1]
 
+    r = None
     try:
         ipfs_uri = app.config['POLYSWARMD'].ipfs_uri
         r = requests.get(ipfs_uri + '/api/v0/object/stat', params={'arg': artifact})
         r.raise_for_status()
     except Exception:
-        logger.exception('Received error stating files from IPFS')
+        logger.exception('Received error stating files from IPFS, got response: %s',
+                         r.content if r is not None else 'None')
         return failure('Could not locate IPFS resource', 400)
 
     # Convert stats to snake_case
