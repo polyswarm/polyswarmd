149577baf5c731ba44e781ffe3dd01b8c933aede
Rob Lathrop <rl@polyswarm.io>
2018-12-18 23:01:31 -0500

    More refactoring.

--- src/polyswarmd/eth.py
+++ src/polyswarmd/eth.py
@@ -104,9 +104,12 @@ def post_transactions():
     # If we don't have a user key, and they are required, start checking the transaction
     if not g.user and app.config['POLYSWARMD'].require_api_key:
         logger.info('Comparing transactions against withdrawal signature')
-        error = check_withdrawal(body['transactions'])
-        if error:
-            return failure(error, 403)
+        try:
+            check_withdrawal(body['transactions'])
+        except ValueError as e:
+            logger.error('Invalid transaction: %s', e)
+        except Exception:
+            logger.exception('Unexpected exception while parsing transaction')
 
     errors = []
     txhashes = []
@@ -163,27 +166,24 @@ def check_withdrawal(transactions):
     Take a list of transactions, and show the set consists of only
     withdrawals from the sidechain.
     """
-    error = None
     if len(transactions) != 1:
         logger.error('Too many transactions to be a withdrawl')
-        return 'multiple transactions requires an api-key'
+        raise ValueError('multiple transactions requires an api-key')
 
     function_hash = g.chain.w3.toHex(tx.data[:4])
     if len(tx.data) != 68 or function_hash != transfer_signature_hash:
-        return 'calling something other than transfer requires an api-key'
+        raise ValueError('calling something other than transfer requires an api-key')
 
-    tx = rlp.decode(bytes.fromhex(transactions[0]), Transaction)
+    tx = rlp.decode(bytes.fromhex(raw_tx), Transaction)
     to = g.chain.w3.toChecksumAddress(tx.to.hex())
     amount = int.from_bytes(tx.data[36:], byteorder='big')
     target = g.chain.w3.toChecksumAddress(g.chain.w3.toHex(tx.data[16:36]))
-
     if g.chain.nectar_token.address != to or tx.value != 0 or tx.network_id != app.config["POLYSWARMD"].chains['side'].chain_id:
-        error = 'sending a transaction to a contract other than the side nectar token requires an api-key'
-    elif target != g.chain.erc20_relay.address or amount <= 0:
-        error = 'transfering NCT to an address other than erc20relay contract requires an api-key'
+        raise ValueError('sending a transaction to a contract other than the side nectar token requires an api-key')
+    if target != g.chain.erc20_relay.address or amount <= 0:
+        raise ValueError('transfering NCT to an address other than erc20relay contract requires an api-key')
 
     logger.info('transaction with no api-key was a withdrawal %s', tx)
-    return error
 
 
 def events_from_transaction(txhash):
