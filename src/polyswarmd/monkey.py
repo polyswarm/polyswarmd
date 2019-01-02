46733afa4b8117238947d7dadff7d67e906c9e7c
Rob Lathrop <rl@polyswarm.io>
2019-01-02 15:07:07 -0500

    Refresh session on ConnectionError

--- src/polyswarmd/monkey.py
+++ src/polyswarmd/monkey.py
@@ -1,6 +1,8 @@
 import web3
 from gevent import monkey
 from requests_futures.sessions import FuturesSession
+from requests.exceptions import ConnectionError
+MAX_RETRIES = 2
 
 session = FuturesSession()
 
@@ -16,11 +18,21 @@ def patch_gevent():
 
 def patch_web3():
     def make_post_request(endpoint_uri, data, *args, **kwargs):
+        global session
         kwargs.setdefault('timeout', 1)
-        future = session.post(endpoint_uri, data=data, *args, **kwargs)
-        response = future.result()
-        response.raise_for_status()
-
-        return response.content
+        tries = 0
+        while tries < MAX_RETRIES:
+                future = session.post(endpoint_uri, data=data, *args, **kwargs)
+            try:
+                response = future.result()
+            except ConnectionError as e:
+                tries += 1
+                logger.error('Connection error: %s', e)
+                # Create new session since the last closed
+                session = FuturesSession()
+                continue
+            response.raise_for_status()
+            return response.content
+        logger.error('unable able to recreate session')
 
     web3.providers.rpc.make_post_request = make_post_request
