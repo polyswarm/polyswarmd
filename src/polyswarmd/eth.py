2cd434f45f258354100628dc46d102171c8caada
Maxwell Koo <mjkoo90@gmail.com>
2019-01-03 17:14:57 -0700

    Get pending txs too in tx count, hopefully will alleviate some nonce desync issues

--- src/polyswarmd/eth.py
+++ src/polyswarmd/eth.py
@@ -72,8 +72,7 @@ def get_syncing():
 @chain
 def get_nonce():
     account = g.chain.w3.toChecksumAddress(g.eth_address)
-
-    return success(g.chain.w3.eth.getTransactionCount(account))
+    return success(g.chain.w3.eth.getTransactionCount(account, 'pending'))
 
 
 @misc.route('/transactions', methods=['POST'])
