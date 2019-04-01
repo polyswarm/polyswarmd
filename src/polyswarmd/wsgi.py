270aa19babd1bd16dc08c5b682e44dfce58a5dd5
Maxwell Koo <mjkoo90@gmail.com>
2019-04-01 12:43:04 -0600

    Allow setting log level via command line and env var

--- src/polyswarmd/wsgi.py
+++ src/polyswarmd/wsgi.py
@@ -1,7 +1,22 @@
 import logging
+import sys
 import os
 
 from polyswarmd.logger import init_logging
-init_logging(os.environ.get('LOG_FORMAT'), logging.INFO)
 
-from polyswarmd import app as application
+
+def app(*args, **kwargs):
+    # Can't directly pass command line arguments via gunicorn, but can pass arguments to callable
+    # https://stackoverflow.com/questions/8495367/using-additional-command-line-arguments-with-gunicorn
+    log_format = os.environ.get('LOG_FORMAT', kwargs.get('log_format', 'text'))
+
+    log_level = os.environ.get('LOG_LEVEL', kwargs.get('log_level', 'WARNING'))
+    log_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(log_level, int):
+        logging.error('Invalid log level')
+        sys.exit(-1)
+
+    init_logging(log_format, log_level)
+
+    from polyswarmd import app as application
+    return application
