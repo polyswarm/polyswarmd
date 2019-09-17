6f07142068301c999c52dd4ebbe715609b553bc9
Rob Lathrop <rl@polyswarm.io>
2019-09-17 11:24:40 -0400

    Handle bool list with leading zeros

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,25 +7,35 @@ from polyswarmd import utils, app
 from polyswarmd.eth import ZERO_ADDRESS
 from tests import client, test_account
 
+
 def test_to_padded_hex():
     assert utils.to_padded_hex("0xabcd").endswith("abcd")
     assert utils.to_padded_hex(15).endswith("f")
     assert utils.to_padded_hex("AAAA").endswith("41414141")
     assert utils.to_padded_hex(b"AAAA").endswith("41414141")
 
+
 def test_bool_list_to_int():
     bool_list = utils.bool_list_to_int([True, True, False, True])
     expected = 11
     assert bool_list == expected
+
 
 def test_int_to_bool_list():
     bool_list = utils.int_to_bool_list(11)
     expected = [True, True, False, True]
     assert bool_list == expected
 
+
 def test_safe_int_to_bool_list():
     bool_list = utils.safe_int_to_bool_list(0, 5)
     expected = [False, False, False, False, False]
+    assert bool_list == expected
+
+
+def test_safe_int_to_bool_list_leading_zeros():
+    bool_list = utils.safe_int_to_bool_list(1, 5)
+    expected = [False, False, False, False, True]
     assert bool_list == expected
 
 def test_state_to_dict(client):
