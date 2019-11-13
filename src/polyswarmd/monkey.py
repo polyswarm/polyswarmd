11f06d39d7d64ee526dcbb9039c5d835ac9616fe
Ben Schmidt <bens@polyswarm.io>
2019-11-13 16:23:31 -0400

    disable monkeypatch

--- src/polyswarmd/monkey.py
+++ src/polyswarmd/monkey.py
@@ -9,7 +9,8 @@ session = FuturesSession(executor=ThreadPoolExecutor(), adapter_kwargs={'max_ret
 
 
 def patch_all():
-    patch_gevent()
+    if not os.getenv('DISABLE_WEBSOCKETS', False):
+        patch_gevent()
     patch_web3()
 
 
