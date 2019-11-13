2d431f4bb69992fc2213472de4efe332b8866d13
Ben Schmidt <bens@polyswarm.io>
2019-11-13 15:19:55 -0400

    attempt fix suggested in web3.js github issue

--- src/polyswarmd/eth.py
+++ src/polyswarmd/eth.py
@@ -335,6 +335,9 @@ def events_from_transaction(txhash, chain):
         while True:
             tx = g.chain.w3.eth.getTransaction(txhash)
             if tx is not None and tx.blockNumber:
+                # fix suggested by https://github.com/ethereum/web3.js/issues/2917#issuecomment-507154487
+                while g.chain.w3.eth.blockNumber - tx.blockNumber < 0:
+                    gevent.sleep(1)
                 receipt = g.chain.w3.eth.getTransactionReceipt(txhash)
                 break
             gevent.sleep(1)
