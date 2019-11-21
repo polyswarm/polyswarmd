9663aaeefcf90af612a93c190056e562944b3a1e
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-11-20 22:11:04 -0800

    Restore `init_logging' in __init__.py

--- src/polyswarmd/__init__.py
+++ src/polyswarmd/__init__.py
@@ -14,6 +14,7 @@ from flask import Flask, g, request
 from flask_caching import Cache
 
 from polyswarmd.config import Config, is_service_reachable, DEFAULT_FALLBACK_SIZE
+from polyswarmd.logger import init_logging
 from polyswarmd.profiler import setup_profiler
 from polyswarmd.response import success, failure, install_error_handlers
 
