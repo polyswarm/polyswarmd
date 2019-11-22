8b9a1d226229d52fa59add00fe24c72ae981098a
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-11-21 17:51:15 -0800

    Add script for linting codebase

---
+++ scripts/lint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+root="${0%/*}/.."
+srcpath="$root/src/polyswarmd"
+
+main() {
+    if [[ "$1" = "-i" ]]; then
+        read -p "Do you want to destructively update files in $srcpath (y/n): " -n1
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            set -euxo pipefail
+            inplace_update
+        fi
+    elif [[ "$1" = "-l" ]]; then
+        set -euxo pipefail
+        lint
+    else
+        cat <<EOF
+Usage:
+ $0 [options]
+
+Options:
+  -l				Lint $srcpath with mypy, isort & yapf.
+  -i				Apply destructive formatting with isort & yapf
+EOF
+    fi
+}
+
+
+lint() {
+    cd "$root"
+
+    mypy && \
+    yapf -r -d "$srcpath" && \
+    isort --recursive --diff "$srcpath"
+
+    exit $?
+}
+
+inplace_update() {
+    cd "$root"
+
+    yapf -r -i "$srcpath" && isort --recursive "$srcpath"
+
+    exit $?
+}
+
+
+( main "${1:--l}" )
--- scripts/pylint.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# cd to repository root directory
-cd "${0%/*}/.."
-
-source venv/bin/activate
-cd src
-python3 $(which pylint) --rcfile=../.pylintrc polyswarmd
