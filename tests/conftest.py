497c7fdfaf721bdb7a896f068cce2ebe982776a0
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2020-01-14 22:25:59 -0800

    Add 'heck' to check response objects in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
 """
    isort:skip_file
 """
+from collections import UserDict
+from collections.abc import Mapping
+
+from string import hexdigits
+import random
+from pathlib import Path
 import os
 import pytest
+import json
 from unittest.mock import patch
 
 import requests.adapters
@@ -19,12 +26,12 @@ def let(obj, **kwargs):
 PRE_INIT_PATCHES = (
     # don't both with patching gevent methods inside pytest
     patch('gevent.monkey.patch_all', return_value=None),
-    # fake out the underlying ipfshttpclient connect
-    patch('ipfshttpclient.connect', return_value=True),
-    # replace requests.adapter's send method
-    patch.object(
-        requests.adapters.HTTPAdapter, 'send', return_value=let(Response(), status_code=200)
-    ),
+    # # fake out the underlying ipfshttpclient connect
+    # patch('ipfshttpclient.connect', return_value=True),
+    # # replace requests.adapter's send method
+    # patch.object(
+    #     requests.adapters.HTTPAdapter, 'send', return_value=let(Response(), status_code=200)
+    # ),
     # set `POLY_WORK` to be 'testing' (if not already set)
     patch('os.getenv', lambda *args, **kwargs: 'testing' if args[0] == 'POLY_WORK' else os.getenv)
 )
@@ -36,7 +43,8 @@ for pa in PRE_INIT_PATCHES:
 # raise an exception. Fixing this (e.g moving stuff outta src/polyswarmd/__init__.py) has been on the
 # todo list for some time, but for now, we just patch up methods which have unsafe side effects to
 # run unit tests without side-effects.
-import polyswarmd.app  # noqa
+import polyswarmd
+from polyswarmd.app import app as _app  # noqa
 
 for pa in PRE_INIT_PATCHES:
     pa.stop()
@@ -46,6 +54,9 @@ for pa in PRE_INIT_PATCHES:
 def community():
     return 'gamma'
 
+@pytest.fixture
+def base_nonce():
+    return random.randint(2**15, 2**16)
 
 @pytest.fixture
 def token_address():
@@ -54,16 +65,87 @@ def token_address():
 
 @pytest.fixture
 def app():
-    yield polyswarmd.app
+    return _app
 
 
 @pytest.fixture
 def client(app):
     app.config['TESTING'] = True
-    yield polyswarmd.app.test_client()
+    yield app.test_client()
 
 
 @pytest.fixture
 def ZERO_ADDRESS():
     from polyswarmd.views.eth import ZERO_ADDRESS
     return ZERO_ADDRESS
+
+
+CHAINCFG = Path('tests/fixtures/config/chain/').resolve()
+
+def _read_chain(chain_name):
+    with open(CHAINCFG.joinpath(f'{chain_name}chain.json')) as ff:
+        cobj = json.load(ff)
+        cobj.update({'chain_name': chain_name})
+        return cobj
+
+
+@pytest.fixture(params=['home'])
+def homechain(request):
+    return _read_chain(request.param)
+
+
+@pytest.fixture(params=['side'])
+def sidechain(request):
+    return _read_chain(request.param)
+
+
+@pytest.fixture(params=['home', 'side'])
+def chain(request):
+    return _read_chain(request.param)
+
+
+class ExpectedProxy(UserDict):
+    """MappingProxy which allows functions as value to overide inner equality checks"""
+
+    def __init__(self, data):
+        if not isinstance(data, Mapping):
+            raise ValueError("Invalid type: %s" % type(data))
+        super().__init__(data.copy())
+
+    @classmethod
+    def fixup(cls, actual, expected):
+        """Checks if `expected` is callable & `expected(actual)` is truthy, returning `actual` or `expected`"""
+        if isinstance(expected, Mapping) and len(expected) == len(actual):
+            return {k: cls.fixup(actual[k], expected[k]) for k in expected}
+        if isinstance(expected, list):
+            return [cls.fixup(actual[i], expected[i]) for i in range(len(expected))]
+        elif callable(expected):
+            if expected(actual):
+                return actual
+            else:
+                return 'EXPECT_CHECK_FAILURE=' + str(actual)
+        return expected
+
+    def __eq__(self, actual):
+        """Checks if ACTUAL is identical to EXPECTED, all funcs in actual are evaluated with ACTUAL 'cousin'"""
+        return actual == self.fixup(actual, expected=self.data)
+
+    # -----------------------------------
+
+    @staticmethod
+    def is_address(addr: str) -> bool:
+        if addr.startswith('0x'):
+            addr = addr[2:]
+        return all(ch in hexdigits for ch in addr.lower())
+
+    @staticmethod
+    def is_positive(num: int) -> bool:
+        try:
+            return num > 0
+        except Exception:
+            return False
+
+
+@pytest.fixture
+def heck():
+    return ExpectedProxy
---
+++ tests/test_suite_internals.py
@@ -0,0 +1,69 @@
+import pytest  # noqa
+
+
+def pytest_generate_tests(metafunc):
+    # This pytest hook that is called once for each test function It takes all of the dicts found in
+    # `TestHeck`, selecting the key that shares the current test function's name & then
+    # parameterizes by those as if they were arguments.
+    name = metafunc.function.__name__
+    metafunc.parametrize(['obj'], [[funcargs] for funcargs in metafunc.cls.params[name]])
+
+
+GOOD = 'good'
+BAD = 'bad'
+F1 = 'first'
+F2 = 'second'
+F3 = 'third'
+N1 = 'Alpha'
+N2 = 'Beta'
+N3 = 'Delta'
+N4 = 'wut'
+
+
+class TestHeck:
+    params = {
+        'test_equal_simple': [
+            {GOOD: 1},
+            {GOOD: lambda x: x == 1},
+            {GOOD: lambda x: x},
+            {GOOD: lambda x: True},
+            {GOOD: lambda x: x}
+        ],
+        'test_equal_deep': [
+            {F1: {F2: {F3: {N1: 1, N2: 'two', N3: [3, 4]}}}},
+            {F1: {F2: {F3: {N1: 1, N2: lambda x: isinstance(x, str), N3: [3, 4]}}}},
+            {F1: {F2: {F3: {N1: 1, N2: 'two', N3: lambda x: len(x) > 0}}}},
+            {F1: {F2: {F3: {N1: lambda x: x == 1, N2: lambda x: isinstance(x, str), N3: [3, 4]}}}},
+            {F1: {F2: {F3: {N1: lambda x: x == 1, N2: lambda x: isinstance(x, str), N3: lambda x: x[0] == 3}}}},
+            {F1: {F2: {F3: lambda d: len(d.keys()) == 3}}}
+        ],
+        'test_not_equal_simple': [
+            {BAD: lambda x: x > 1},
+            {BAD: lambda x: x == float('nan')},
+            {BAD: lambda x: x == -10123123},
+            {BAD: lambda x: not x},
+            {BAD: 2}
+        ],
+        'test_not_equal_deep': [
+            {F1: {F2: {F3: {N1: 2, N2: 'two', N3: [3, 4]}}}},
+            {F1: {F2: {F3: {N1: 1, N2: 'two', N3: [4, 3]}}}},
+            {F1: {F2: {F3: {N1: 1, N2: lambda x: isinstance(x, int), N3: [3, 4]}}}},
+            {F1: {F2: {F3: {N1: 1, N2: 'two', N3: lambda x: len(x) == 0}}}},
+            {F1: {F2: {F3: {N1: lambda x: x == 1, N2: lambda x: isinstance(x, str), N3: [4, 3]}}}},
+            {F1: {F2: {F3: {N1: lambda x: x == 2, N2: lambda x: isinstance(x, str), N3: [3, 4]}}}},
+            {F1: {F2: {F3: lambda d: len(d.keys()) == 0}}},
+            {F1: {F2: {F3: 1}}}
+        ],
+    }
+
+    def test_equal_simple(self, obj, heck):
+        assert {GOOD: 1} == heck(obj)
+
+    def test_equal_deep(self, obj, heck):
+        assert {F1: {F2: {F3: {N1: 1, N2: 'two', N3: [3, 4]}}}} == heck(obj)
+
+    def test_not_equal_simple(self, obj, heck):
+        assert {BAD: 1} != heck(obj)
+
+    def test_not_equal_deep(self, obj, heck):
+        assert {F1: {F2: {F3: {N1: 1, N2: 'two', N3: [3, 4]}}}} != heck(obj)
